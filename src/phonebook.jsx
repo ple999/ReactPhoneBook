b6d2485148fb610d444a98cbe49c1d18edb42d70
Ple999 <72064291+ple999@users.noreply.github.com>
2024-06-14 20:23:42 +0700

    2.0

--- src/phonebook.jsx
+++ src/phonebook.jsx
@@ -1,123 +1,153 @@
-import { useState,useEffect } from "react";
-import PhoneBookService from './service/phoneBookService.js'
+import { useState, useEffect } from "react";
+import PhoneBookService from "./service/phoneBookService.js";
 
-const Header=({text})=>{
-    return(
-        <h1>{text}</h1>
-    )
-}
+const Header = ({ text }) => {
+  return <h1>{text}</h1>;
+};
 
-const List=({list,func})=>{
+const List = ({ list, func }) => {
+  return (
+    <>
+      {list.map((v) => {
+        return (
+          <>
+            <p>
+              {v.name} {v.number}
+            </p>
+            <button
+              onClick={() => {
+                func(v);
+              }}
+            >
+              Delete
+            </button>
+          </>
+        );
+      })}
+    </>
+  );
+};
 
-    return(
-        <>
-            {list.map((v)=>{
-               return(
-               <> 
-               <p>{v.name} {v.number}</p>
-               <button onClick={()=>{func(v)}}>Delete</button>
-               </>) 
-            })}
-        </>
-    )
-}
+const AppPhoneBook = () => {
+  let [person, newPerson] = useState([]);
+  let [personInput, newPersonInput] = useState("");
+  let [numberInput, newNumberInput] = useState("");
+  let [filterInput, newFilterInput] = useState("");
 
+  let filteredPerson = person.filter((v) => {
+    return v.name.includes(filterInput);
+  });
 
-const AppPhoneBook=()=>{
-    let [person,newPerson]=useState([])
+  const inputOnchange = (e, selfunc) => {
+    selfunc(e.target.value);
+  };
 
-    let [personInput,newPersonInput]=useState('');
-    let [numberInput,newNumberInput]=useState('');
-    let [filterInput,newFilterInput]=useState('');
+  const deletePhoneBook = (v) => {
+    PhoneBookService.deletePhoneBook(v).then((res) => {
+      console.log(person);
+      newPerson(
+        person.filter((v) => {
+          return v.id != res.data.id;
+        }),
+      );
+      console.log("Data Deleted");
+    });
+  };
 
-    let filteredPerson=person.filter((v)=>{
-        return v.name.includes(filterInput);
-    })
+  useEffect(() => {
+    PhoneBookService.getPhoneBook()
+      .then((res) => {
+        newPerson(res.data);
+      })
+      .catch(() => {
+        console.log("Get Error");
+      });
+  }, []);
 
-    const inputOnchange=(e,selfunc)=>{
-        selfunc(e.target.value);
-    }
-
-    const deletePhoneBook=(v)=>{
-        PhoneBookService.deletePhoneBook(v).then((res)=>{
-            console.log(person);
-            newPerson(person.filter((v)=>{
-                return v.id!=res.data.id
-            }))
-            console.log('Data Deleted');
-        })
-    }
-
-    useEffect(()=>{
-        PhoneBookService.getPhoneBook().then((res)=>{
-            newPerson(res.data)
-        }).catch(()=>{
-            console.log('Get Error');
-        })
-    },[])
-
-
-
-    return(
-        <>
-        <Header text='Phonebook'/>
-        <span>Filter by name :</span><input onChange={(e)=>{
-            inputOnchange(e,newFilterInput)
-        }}/>
-        <br/>
-        <br/>
-        <br/>
-        <Header text='Add a User'/>
-        <form method="post" onSubmit={(e)=>{
-            e.preventDefault();
-            let personObject={
-                name:personInput,
-                number:numberInput,
-                id:person.length+1
+  return (
+    <>
+      <Header text="Phonebook" />
+      <span>Filter by name :</span>
+      <input
+        onChange={(e) => {
+          inputOnchange(e, newFilterInput);
+        }}
+      />
+      <br />
+      <br />
+      <br />
+      <Header text="Add a User" />
+      <form
+        method="post"
+        onSubmit={(e) => {
+          e.preventDefault();
+          let personObject = {
+            name: personInput,
+            number: numberInput,
+            id: person.length + 1,
+          };
+          if (
+            person.find((v) => {
+              return v.name == personInput;
+            })
+          ) {
+            if (
+              confirm(
+                `data with the name ${personInput} already Exist Do you want to Replace The Current Data?`,
+              )
+            ) {
+              PhoneBookService.updatePhoneBook(
+                person.filter((v) => {
+                  return v.name == personInput;
+                })[0],
+                personObject,
+              ).then((res) => {
+                let replacedIndex = person.findIndex((v) => {
+                  return v.name == personInput;
+                });
+                let updatedPerson = [...person];
+                updatedPerson[replacedIndex] = res.data;
+                newPerson(updatedPerson);
+              });
             }
-            if(person.find((v)=>{
-                return v.name==personInput
-            })){
-                if(confirm(`data with the name ${personInput} already Exist Do you want to Replace The Current Data?`)){
-                    PhoneBookService.updatePhoneBook((person.filter((v)=>{
-                        return v.name==personInput
-                    }))[0],personObject).then((res)=>{
-                        let replacedIndex =person.findIndex((v)=>{
-                            return v.name==personInput
-                        })
-                        let updatedPerson=[...person];
-                        updatedPerson[replacedIndex]=res.data;
-                        newPerson(updatedPerson);
-                    })
-                }
-
-            }else{
-                PhoneBookService.postPhoneBook(personObject).then((res)=>{
-                    console.log('Posted')
-                    newPerson(person.concat(res.data));
-                }).catch(()=>{
-                    console.log('Post Failed')
-                })
-                newNumberInput('');
-                newPersonInput('');
-            }
-
-
-
-        }}>
-        <span>Name   :</span><input onChange={(e)=>{inputOnchange(e,newPersonInput)}} /><br/>
-        <span>Number :</span><input onChange={(e)=>{inputOnchange(e,newNumberInput)}} /><br/>
+          } else {
+            PhoneBookService.postPhoneBook(personObject)
+              .then((res) => {
+                console.log("Posted");
+                newPerson(person.concat(res.data));
+                console.log(person);
+              })
+              .catch(() => {
+                console.log("Post Failed");
+              });
+            newNumberInput("");
+            newPersonInput("");
+          }
+        }}
+      >
+        <span>Name :</span>
+        <input
+          onChange={(e) => {
+            inputOnchange(e, newPersonInput);
+          }}
+        />
+        <br />
+        <span>Number :</span>
+        <input
+          onChange={(e) => {
+            inputOnchange(e, newNumberInput);
+          }}
+        />
+        <br />
         <button type="submit">Add Person</button>
-        </form>
-        <br/>
-        <br/>
-        <br/>
-        <Header text='Number' />
-        <List list={filteredPerson} func={deletePhoneBook}/>
-        </>
-    )
+      </form>
+      <br />
+      <br />
+      <br />
+      <Header text="Number" />
+      <List list={filteredPerson} func={deletePhoneBook} />
+    </>
+  );
+};
 
-}
-
-
-export default AppPhoneBook
+export default AppPhoneBook;
--- src/service/phoneBookService.js
+++ src/service/phoneBookService.js
@@ -1,27 +1,26 @@
-import axios from "axios"
-const url='https://cmcv4p-3000.csb.app/api/persons'
+import axios from "axios";
+const url = "https://3q2cv4-7000.csb.app/api/persons";
 
+const getPhoneBook = () => {
+  return axios.get(url);
+};
 
-const getPhoneBook=()=>{
-   return axios.get(url)
-}
+const postPhoneBook = (obj) => {
+  return axios.post(url, obj);
+};
 
-const postPhoneBook=(obj)=>{
-  return  axios.post(url,obj)
-}
+const deletePhoneBook = (obj) => {
+  return axios.delete(`${url}/${obj.id}`);
+};
 
-const deletePhoneBook=(obj)=>{
-   return axios.delete(`${url}/${obj.id}`);
-}
+const updatePhoneBook = (obj, newObj) => {
+  newObj.id = obj.id;
+  return axios.put(`${url}/${obj.id}`, newObj);
+};
 
-const updatePhoneBook=(obj,newObj)=>{
-    newObj.id=obj.id;
-    return axios.put(`${url}/${obj.id}`,newObj)
-}
-
-export default{
-    getPhoneBook,
-    postPhoneBook,
-    deletePhoneBook,
-    updatePhoneBook
-}
+export default {
+  getPhoneBook,
+  postPhoneBook,
+  deletePhoneBook,
+  updatePhoneBook,
+};
